43c3c3e023ffe3591f2c1ba7723c8bf8267aee5a
addalaraed <addala.raed@gmail.com>
2025-01-11 18:21:05 +0100

    chore: add font awesome icons in shared

---
+++ el-marchi-ui/apps/el-marchi-ui/src/shared/font-awesome-icons.ts
@@ -0,0 +1,5 @@
+import type { IconDefinition } from '@fortawesome/angular-fontawesome';
+import { faUser as faUserRegular } from '@fortawesome/free-regular-svg-icons';
+import { faUser as faUserSolid } from '@fortawesome/free-solid-svg-icons';
+
+export const fontAwesomeIcons: IconDefinition[] = [faUserRegular, faUserSolid];
