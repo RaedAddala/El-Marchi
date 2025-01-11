07712f6f51c007ed95a12ce14baf515fc6549921
addalaraed <addala.raed@gmail.com>
2025-01-11 20:16:37 +0100

    chore: add fontAwesomeIcons library to the app

--- el-marchi-ui/apps/el-marchi-ui/src/app/app.component.ts
+++ el-marchi-ui/apps/el-marchi-ui/src/app/app.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject, type OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FaConfig, FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { fontAwesomeIcons } from '../shared/font-awesome-icons';
 
 @Component({
-  imports: [RouterModule],
+  imports: [RouterModule,FontAwesomeModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   standalone: true,
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'el-marchi-ui';
+
+  private readonly faIconLibrary = inject(FaIconLibrary);
+  private readonly faConfig = inject(FaConfig);
+
+  ngOnInit(): void {
+    this.initFontAwesome();
+  }
+  initFontAwesome() {
+    this.faConfig.defaultPrefix = 'far';
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
+
 }
--- el-marchi-ui/apps/el-marchi-ui/src/shared/font-awesome-icons.ts
+++ el-marchi-ui/apps/el-marchi-ui/src/shared/font-awesome-icons.ts
@@ -1,5 +1,4 @@
 import type { IconDefinition } from '@fortawesome/angular-fontawesome';
-import { faUser as faUserRegular } from '@fortawesome/free-regular-svg-icons';
-import { faUser as faUserSolid } from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-regular-svg-icons';
 
-export const fontAwesomeIcons: IconDefinition[] = [faUserRegular, faUserSolid];
+export const fontAwesomeIcons: IconDefinition[] = [faUser];
