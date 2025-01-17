b16c346b33c9a9db8eeda1bef098616beec66894
addalaraed <addala.raed@gmail.com>
2025-01-17 11:26:47 +0100

    chore: UI improvements on the navbar

--- el-marchi-ui/apps/el-marchi-ui/src/app/layout/navbar/navbar.component.ts
+++ el-marchi-ui/apps/el-marchi-ui/src/app/layout/navbar/navbar.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-navbar',
-  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
   standalone: true,
+  imports: [CommonModule, RouterModule, FontAwesomeModule],
 })
 export class NavbarComponent {}
