7248fcc0ff07b5739585381a1b9056c52f8977a4
addalaraed <addala.raed@gmail.com>
2025-01-17 11:59:25 +0100

    feat: added footer to the layout

--- el-marchi-ui/apps/el-marchi-ui/src/app/layout/footer/footer.component.ts
+++ el-marchi-ui/apps/el-marchi-ui/src/app/layout/footer/footer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-footer',
-  imports: [CommonModule],
+  imports: [CommonModule,FontAwesomeModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css',
   standalone: true,
--- el-marchi-ui/apps/el-marchi-ui/src/shared/font-awesome-icons.ts
+++ el-marchi-ui/apps/el-marchi-ui/src/shared/font-awesome-icons.ts
@@ -2,11 +2,27 @@ import type { IconDefinition } from '@fortawesome/angular-fontawesome';
 import { faUser as faRUser } from '@fortawesome/free-regular-svg-icons';
 import {
   faCartShopping,
+  faTruckFast,
   faUser as faSUser,
 } from '@fortawesome/free-solid-svg-icons';
+import {
+  faYoutube,
+  faTwitter,
+  faXTwitter,
+  faFacebook,
+  faLinkedin,
+  faInstagram,
+} from '@fortawesome/free-brands-svg-icons';
 
 export const fontAwesomeIcons: IconDefinition[] = [
   faRUser,
   faSUser,
   faCartShopping,
+  faTruckFast,
+  faYoutube,
+  faTwitter,
+  faXTwitter,
+  faFacebook,
+  faLinkedin,
+  faInstagram,
 ];
