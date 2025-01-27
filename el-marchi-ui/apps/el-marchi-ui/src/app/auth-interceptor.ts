ea675826a50711a4844b25ab0f5b89cfc70dbca2
Ala Eddine Achach <122971085+Ala-Eddine-Achach@users.noreply.github.com>
2025-01-27 16:56:34 +0100

    add interceptor to add token for all http request

---
+++ el-marchi-ui/apps/el-marchi-ui/src/app/auth-interceptor.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import {AuthService} from "@features/auth/auth.service";
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor(private authService: AuthService) {}
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    // Get the token from your AuthService
+    const token = this.authService.getToken();
+
+    // Clone the request and add the Authorization header
+    if (token) {
+      const cloned = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      return next.handle(cloned);
+    }
+
+    // If no token, proceed with the original request
+    return next.handle(req);
+  }
+}
